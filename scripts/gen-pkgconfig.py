3de4cb205a0914d95411e2db9d7de88c15a3e6b8
Brendan Long <self@brendanlong.com>
2014-05-15 12:18:13 -0500

    Fix pkg-config stuff.

--- scripts/gen-pkgconfig.py
+++ scripts/gen-pkgconfig.py
@@ -9,13 +9,19 @@ VERSION = [str(i) for i in (1, 0, 0)]
 VERSION_STR = ".".join(VERSION)
 MAJOR_VERSION = ".".join(VERSION[:2])
 
+# Remove annoying tup stuff from path
+if "@tupjob" in CURRENT_DIR:
+    begin = CURRENT_DIR.find("@tupjob")
+    begin = CURRENT_DIR.find("/", begin)
+    CURRENT_DIR = CURRENT_DIR[begin:]
+
 with open("../pkgconfig/%s-%s.pc.in" % (NAME, MAJOR_VERSION), "r") as f:
     template = f.read();
+#with open("../pkgconfig/%s-%s.pc" % (NAME, MAJOR_VERSION), "w") as f:
+#    f.write(string.Template(template).substitute(name=NAME, prefix=PREFIX,
+#        version=VERSION_STR, major_version=MAJOR_VERSION, include="/include",
+#        lib="/lib"))
 with open("../pkgconfig/%s-%s.pc" % (NAME, MAJOR_VERSION), "w") as f:
-    f.write(string.Template(template).substitute(name=NAME, prefix=PREFIX,
-        version=VERSION_STR, major_version=MAJOR_VERSION, include="/include",
-        lib="/lib"))
-with open("../pkgconfig/%s-uninstalled-%s.pc" % (NAME, MAJOR_VERSION), "w") as f:
     f.write(string.Template(template).substitute(name=NAME,
         prefix=os.path.join(CURRENT_DIR, "..", "src"),
         version=VERSION_STR, major_version=MAJOR_VERSION, include="", lib=""))
