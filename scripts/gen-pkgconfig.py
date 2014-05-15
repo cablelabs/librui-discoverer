4f3e3b5985ff24b1696f5d46508154433e1e0098
Brendan Long <self@brendanlong.com>
2014-05-15 11:58:08 -0500

    Add pkg-config files.

---
+++ scripts/gen-pkgconfig.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import os
+import string
+
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+NAME = "rui-discoverer"
+PREFIX = "/usr/local"
+VERSION = [str(i) for i in (1, 0, 0)]
+VERSION_STR = ".".join(VERSION)
+MAJOR_VERSION = ".".join(VERSION[:2])
+
+with open("../pkgconfig/%s-%s.pc.in" % (NAME, MAJOR_VERSION), "r") as f:
+    template = f.read();
+with open("../pkgconfig/%s-%s.pc" % (NAME, MAJOR_VERSION), "w") as f:
+    f.write(string.Template(template).substitute(name=NAME, prefix=PREFIX,
+        version=VERSION_STR, major_version=MAJOR_VERSION, include="/include",
+        lib="/lib"))
+with open("../pkgconfig/%s-uninstalled-%s.pc" % (NAME, MAJOR_VERSION), "w") as f:
+    f.write(string.Template(template).substitute(name=NAME,
+        prefix=os.path.join(CURRENT_DIR, "..", "src"),
+        version=VERSION_STR, major_version=MAJOR_VERSION, include="", lib=""))
+
